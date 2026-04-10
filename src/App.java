687ab93f242c063c7763fdc60347c51b6753567d
Edwin Pintado <epintador@est.ups.edu.ec>
2026-04-10 11:46:38 -0500

    UPS : EDWIN TAREA

--- src/App.java
+++ src/App.java
@@ -5,6 +5,11 @@ public class App {
         int [] array = {1,2,3,4,5};
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
+            System.out.println(num);
         }
+        for(int i : array) {
+            System.out.println(i);
+        }
+
     }
 }
