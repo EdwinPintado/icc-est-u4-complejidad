c9b5aec5fef1a97a83071c37860fd6e54a9dccbb
Edwin Pintado <epintador@est.ups.edu.ec>
2026-04-10 11:40:19 -0500

    UPS edwin tareas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int num = 10;
+        int [] array = {1,2,3,4,5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
